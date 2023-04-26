56a4087f128c07d637a6d362a10cd0827cd5b95a
killeraw50 <xxmarkc0xx@gmail.com>
2023-04-26 10:28:53 +0800

    used-objects

--- library/javascript.js
+++ library/javascript.js
@@ -1,64 +1,54 @@
-let myLibrary = [];
-let addButton = document.getElementById('add-book');
-let popupForm = document.getElementById('popup-form');
-let cancelButton = document.getElementById('cancel-button');
-
-function Book(title, author, read) {
-    this.title = title;
-    this.author = author;
-    this.read = read;
-}
-
-function addBookToLibrary() {
-    let title = document.getElementById('title').value;
-    let author = document.getElementById('author').value;
-    let read = document.getElementById('read').checked;
-    let book = new Book(title, author, read);
-    myLibrary.push(book);
-    displayLibrary();
-}
-
-function displayLibrary() {
-    let displayBooks = document.getElementById('library');
-    displayBooks.innerHTML = "";
-    for(let i = 0; i < myLibrary.length; i++) {
-        let book = myLibrary[i];
-        let bookElement = document.createElement("div");
-        bookElement.textContent = book.title + " by " + book.author;
-        displayBooks.appendChild(bookElement);
-        let readButton = document.createElement('button');
-        let isRead = book.read;
-        readButton.textContent = isRead ? "Read" : "Not Read";
-        readButton.classList.add(isRead ? "readTrue" : "readFalse");
-        readButton.addEventListener('click' , () => {
-            isRead = !isRead;
-            readButton.textContent = isRead ? "Read" : "Not Read";
-            readButton.classList.toggle("readTrue");
-            readButton.classList.toggle("readFalse");
-            myLibrary[i].read = isRead;
-        })
-        let removeButton = document.createElement('button');
-        removeButton.textContent = "Remove";
-        removeButton.addEventListener('click', () => {
-            myLibrary.splice(i, 1);
-            displayLibrary();
-        })
-        bookElement.appendChild(readButton);
-        bookElement.appendChild(removeButton);
-        displayBooks.appendChild(bookElement);
+const library = {
+    myLibrary: [],
+    addBookToLibrary(title, author, read) {
+        const book = {title, author, read};
+        this.myLibrary.push(book);
+        this.displayLibrary();
+    },
+    removeBook(i) {
+        this.myLibrary.splice(i, 1);
+        this.displayLibrary();
+    },
+    toggleRead(i) {
+        this.myLibrary[i].read = !this.myLibrary[i].read;
+        this.displayLibrary();
+    },
+    displayLibrary() {
+        const displayBooks = document.getElementById("library");
+        displayBooks.innerHTML = "";
+        this.myLibrary.forEach((book, i) => {
+            const bookElement = document.createElement("div");
+            bookElement.textContent = `${book.title} by ${book.author}`;
+  
+            const readButton = document.createElement("button");
+            readButton.textContent = book.read ? "Read" : "Not Read";
+            readButton.classList.add(book.read ? "read" : "notRead");
+            readButton.addEventListener("click" , () => this.toggleRead(i));
+  
+            const removeButton = document.createElement("button");
+            removeButton.textContent = "Remove Book";
+            removeButton.addEventListener("click", () => this.removeBook(i));
+  
+            bookElement.appendChild(readButton);
+            bookElement.appendChild(removeButton);
+            displayBooks.appendChild(bookElement);
+        });
     }
-}
-
-addButton.addEventListener('click', () => {
-    popupForm.classList.toggle('show');
-});
-
-cancelButton.addEventListener('click', () => {
-    popupForm.classList.remove('show');
-})
-
-popupForm.addEventListener('submit', (event) => {
+};
+  
+const addButton = document.getElementById("add-book");
+const popupForm = document.getElementById("popup-form");
+const cancelButton = document.getElementById("cancel-button");
+  
+addButton.addEventListener("click", () => popupForm.classList.toggle("show"));
+  
+cancelButton.addEventListener("click", () => popupForm.classList.remove("show"));
+  
+popupForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    addBookToLibrary();
-    popupForm.classList.remove('show');
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const read = document.getElementById("read").checked;
+    library.addBookToLibrary(title, author, read);
+    popupForm.classList.remove("show");
 });
