e27e7240c007c6c032cde108a4b988f48a8ae0cc
killeraw50 <xxmarkc0xx@gmail.com>
2023-04-27 14:48:11 +0800

    refractoring

--- tic-tac-toe/game.js
+++ tic-tac-toe/game.js
@@ -31,43 +31,32 @@ function playerClick() {
 playerClick();
 function playerWin() {
     for(let i = 0; i < cells.length; i += 3) {
-        if(cells[i].textContent !== "" &&
-            cells[i].textContent === cells[i + 1].textContent &&
-            cells[i + 1].textContent === cells[i + 2].textContent) {
-                gameFinished = true;
+        if(cells[i].textContent !== "" && cells[i].textContent === cells[i + 1].textContent && cells[i + 1].textContent === cells[i + 2].textContent) {
+            gameFinished = true;
             return true;
         }
     }
     for(let i = 0; i < 3; i ++) {
-        if(cells[i].textContent !== "" &&
-            cells[i].textContent === cells[i + 3].textContent &&
-            cells[i + 3].textContent === cells[i + 6].textContent) {
-                gameFinished = true;
+        if(cells[i].textContent !== "" && cells[i].textContent === cells[i + 3].textContent && cells[i + 3].textContent === cells[i + 6].textContent) {
+            gameFinished = true;
             return true;
             }
     }
-    if(cells[0].textContent !== "" &&
-        cells[0].textContent === cells[4].textContent &&
-        cells[4].textContent === cells[8].textContent) {
-            gameFinished = true;
+    if(cells[0].textContent !== "" && cells[0].textContent === cells[4].textContent && cells[4].textContent === cells[8].textContent) {
+        gameFinished = true;
         return true;
         }
-    if(cells[2].textContent !== "" &&
-        cells[2].textContent === cells[4].textContent &&
-        cells[4].textContent === cells[6].textContent) {
-            gameFinished = true;
+    if(cells[2].textContent !== "" && cells[2].textContent === cells[4].textContent && cells[4].textContent === cells[6].textContent) {
+        gameFinished = true;
         return true;
         }
     return false;
 }
 resetButton.addEventListener("click", () => {
-    resetGame();
-})
-function resetGame() {
     cells.forEach(cell => {
         cell.textContent = "";
-      });
-      rounds = 1;
-      gameFinished = false;
-      player.textContent = "Player 1 turn";
-}
+    });
+    rounds = 1;
+    gameFinished = false;
+    player.textContent = "Player 1 turn";
+})
